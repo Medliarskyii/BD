8c250a54acbdaa2d9ecf62abcca9cb71d72eb2ba
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-11-09 18:15:45 +0200

    Add files via upload

---
+++ SocialWinnFormApp/Profiles/PostProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MongoDTO;
+using SocialWinFormApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWinFormApp.Profiles
+{
+    public class PostProfile:Profile
+    {
+        public PostProfile()
+        {
+            CreateMap<PostDTO, AppPostModel>()
+                .ForMember(dest => dest.Body, scr => scr.MapFrom(m => m.Body))
+                .ForMember(dest => dest.Likes, scr => scr.MapFrom(m => m.Likes.Count()))
+                .ForMember(dest => dest.Dislikes,scr => scr.MapFrom(m=>m.Dislikes.Count()))
+                .ForMember(dest => dest.Title, scr => scr.MapFrom(m => m.Title))
+                .ForMember(dest => dest.PostId,scr => scr.MapFrom(m => m.PostId))
+                ;
+        }
+    }
+}
---
+++ SocialWinnFormApp/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MongoDTO;
+using SocialWinFormApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWinFormApp.Profiles
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserDTO, AppUserModel>().ReverseMap();
+            CreateMap<List<UserDTO>, List<AppUserModel>>().ReverseMap();
+        }
+    }
+}
