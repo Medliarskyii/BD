e0f88a9580484d7995a196b75499bb31aae2b189
Medliarskyii <63199632+Medliarskyii@users.noreply.github.com>
2020-11-09 18:13:48 +0200

    Add files via upload

---
+++ SocialWinnFormApp/Models/AppPostModel.cs
@@ -0,0 +1,25 @@
+﻿using MongoDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWinFormApp.Models
+{
+    public class AppPostModel
+    {
+        public int PostId { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Dislikes { get; set; }
+        public List<CommentDTO> Comments { get; set; }
+    }
+}
---
+++ SocialWinnFormApp/Models/AppUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialWinFormApp.Models
+{
+    public class AppUserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+
+        public string UserLastName { get; set; }
+
+        public List<string> Interests { get; set; }
+
+        public List<int> FollowedIdList { get; set; }
+    }
+}
